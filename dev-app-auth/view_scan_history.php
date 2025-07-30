040c9cfd980430e3a09fc9b2ec85aeebd0c37761
azachariah <zachariahadriel@gmail.com>
2025-07-30 15:36:49 -0400

    suppress deprecated errors

--- dev-app-auth/view_scan_history.php
+++ dev-app-auth/view_scan_history.php
@@ -3,7 +3,7 @@
 session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_DEPRECATED);
 
 // Include required files
 require_once('path.inc');
